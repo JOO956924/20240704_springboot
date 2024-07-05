8336ac3b18c5ef00a9130d504ec95f0892034a6c
JOO956924 <wndbs000@gmail.com>
2024-07-05 09:30:55 +0900

    init

---
+++ ex1/src/main/java/com/example/ex1/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.ex1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// SSR(Server Side Rendering) 웹페이지
+@Controller
+@RequestMapping("/")
+public class HelloController {
+  // view를 위한 Template Engine이 라이브러리가 추가 되어 있지 않는 경우는 static활용
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello.html";
+  }
+}
---
+++ ex1/src/main/java/com/example/ex1/dto/Foo.java
@@ -0,0 +1,13 @@
+package com.example.ex1.dto;
+
+import lombok.*;
+
+@Data
+//@Setter
+//@Getter
+//@NoArgsConstructor
+//@ToString
+public class Foo {
+  private String favorite;
+  private int legs;
+}
