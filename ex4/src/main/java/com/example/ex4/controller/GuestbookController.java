ed673ee8750ef162183259f3790c35b8590360c9
JOO956924 <wndbs000@gmail.com>
2024-07-12 15:53:02 +0900

    2024 07 12

--- ex4/src/main/java/com/example/ex4/controller/GuestbookController.java
+++ ex4/src/main/java/com/example/ex4/controller/GuestbookController.java
@@ -1,13 +1,17 @@
 package com.example.ex4.controller;
 
+import com.example.ex4.dto.GuestbookDTO;
 import com.example.ex4.dto.PageRequestDTO;
 import com.example.ex4.service.GuestbookService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -18,9 +22,21 @@ public class GuestbookController {
   private final GuestbookService guestbookService;
 
   @GetMapping({"", "/", "/list"})
-  public String list(Model model) {
+  public String list(Model model, PageRequestDTO pageRequestDTO) {
     model.addAttribute("pageResultDTO",
-        guestbookService.getList(new PageRequestDTO()));
+        guestbookService.getList(pageRequestDTO));
     return "/guestbook/list";
+  }
+
+  @GetMapping("/register")
+  public void registerGet() { log.info("register get.....");}
+
+  @PostMapping("/register")
+  public String registerPost(GuestbookDTO guestbookDTO, RedirectAttributes ra) {
+    log.info("register post.........");
+    Long gno = guestbookService.register(guestbookDTO);
+    ra.addFlashAttribute("msg", gno);
+
+    return "redirect:/guestbook/list";
   }
 }
