ac95462324c3714cddba370631d25bae382bc424
JOO956924 <wndbs000@gmail.com>
2024-07-19 15:07:31 +0900

    2024 07 19

---
+++ ex5/src/main/java/com/example/ex5/Ex5Application.java
@@ -0,0 +1,18 @@
+package com.example.ex5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.ex5.repository")
+public class Ex5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex5Application.class, args);
+		System.out.println("http://localhost:8080/ex5");
+	}
+
+}
