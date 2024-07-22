2f84e5862ab84607125c34629a1ed3594a02876f
JOO956924 <wndbs000@gmail.com>
2024-07-22 16:34:44 +0900

    2024 07 22

---
+++ ex5/src/main/java/com/example/ex5/dto/BoardDTO.java
@@ -0,0 +1,20 @@
+package com.example.ex5.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data //@Data=@ToString+@Getter+@Setter+RequiredArgsConstructor+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDTO {
+  private Long bno;
+  private String title;
+  private String content;
+  private String writerEmail;
+  private String writerName;
+  private LocalDateTime regDate;
+  private LocalDateTime modDate;
+  private int replyCount;
+}
---
+++ ex5/src/main/java/com/example/ex5/service/BoardServiceImpl.java
@@ -0,0 +1,73 @@
+package com.example.ex5.service;
+
+import com.example.ex5.dto.BoardDTO;
+import com.example.ex5.dto.PageRequestDTO;
+import com.example.ex5.dto.PageResultDTO;
+import com.example.ex5.entity.Board;
+import com.example.ex5.entity.Member;
+import com.example.ex5.repository.BoardRepository;
+import com.example.ex5.repository.ReplyRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+  private final BoardRepository boardRepository;
+  private final ReplyRepository replyRepository;
+
+  @Override
+  public Long register(BoardDTO boardDTO) {
+    log.info("register.....");
+    Board board = dtoToEntity(boardDTO);
+    boardRepository.save(board);
+    return board.getBno();
+  }
+
+  @Override
+  public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+    Pageable pageable = pageRequestDTO.getPageable(Sort.by("bno").descending());
+    Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+    Function<Object[], BoardDTO> fn = new Function<Object[], BoardDTO>() {
+      @Override
+      public BoardDTO apply(Object[] obj) {
+        return entityToDTO((Board) obj[0], (Member) obj[1], (Long) obj[2]);
+      }
+    };
+    return new PageResultDTO<>(result, fn);
+  }
+
+  @Override
+  public BoardDTO get(Long bno) {
+    Object result = boardRepository.getboardByBno(bno);
+    Object[] arr = (Object[]) result;
+    return entityToDTO((Board) arr[0], (Member) arr[1], (Long) arr[2]);
+  }
+
+  @Override
+  public void modify(BoardDTO boardDTO) {
+    Optional<Board> result = boardRepository.findById(boardDTO.getBno());
+    if (result.isPresent()) {
+      Board board = result.get();
+      board.changeTitle(boardDTO.getTitle());
+      board.changeContent(boardDTO.getContent());
+      boardRepository.save(board);
+    }
+  }
+
+  @Override
+  @Transactional
+  public void removeWithReplies(BoardDTO boardDTO) {
+    replyRepository.deleteById(boardDTO.getBno());
+    boardRepository.deleteById(boardDTO.getBno());
+  }
+}
